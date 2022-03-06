2f08dbe378543eeffe54d9fcb4c24864b1bf718a
Facundo Lezcano <facu.lezca@gmail.com>
2022-03-06 01:43:03 -0300

    Switch to Bootstrap 5

--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -3,18 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    LoginRoutingModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-  ],
+  imports: [CommonModule, LoginRoutingModule],
 })
 export class LoginModule {}
